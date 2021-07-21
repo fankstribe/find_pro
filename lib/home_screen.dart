091d3afc6e9969433e60dd10431ed1dc11bf5947
bigmama80 <fankstribe@gmail.com>
2021-07-21 12:42:08 +0200

    init

---
+++ lib/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.black,),
+            onPressed: () => {},
+          ),
+           
+        ),
+        backgroundColor: Color(0xffF4F2F3),
+        body: Container(
+          alignment: Alignment.topCenter,
+          height: MediaQuery.of(context).size.height * 0.30,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(35.0),
+              bottomRight: Radius.circular(35.0)
+            )
+          ),
+          child: Text("Ciao"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:find_pro/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomeScreen(),));
+}
+
+
+
+
